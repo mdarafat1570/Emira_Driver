bc60c360d9320b25da21629a5523b93ea6883b2a
MD Arafat Mia <arafat.softifybd@gmail.com>
2025-08-05 23:33:29 +0600

    UPdate on keystore

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -34,14 +34,15 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyAW7IuhHma-RyQqeUqzkOgENONIY4AAaf8',
-    appId: '1:965320825406:ios:cedff9f060ec4cea1ecf6f',
-    messagingSenderId: '965320825406',
-    projectId: 'emiraride-4ee4d',
-    storageBucket: 'emiraride-4ee4d.firebasestorage.app',
-    androidClientId: '965320825406-8hb5b1q2jl19lq8l64bt8253kb2ugt4f.apps.googleusercontent.com',
-    iosClientId: '965320825406-nos0nlcsrjp6i2mof0pksum3vd6j9csc.apps.googleusercontent.com',
-    iosBundleId: 'com.ovosolution.ovoridedriver',
-  );
+  apiKey: 'AIzaSyAW7IuhHma-RyQqeUqzkOgENONIY4AAaf8',
+  appId: '1:965320825406:ios:24984f953bdc59101ecf6f',
+  messagingSenderId: '965320825406',
+  projectId: 'emiraride-4ee4d',
+  storageBucket: 'emiraride-4ee4d.firebasestorage.app',
+  androidClientId: '965320825406-8hb5b1q2jl19lq8l64bt8253kb2ugt4f.apps.googleusercontent.com',
+  iosClientId: '965320825406-rfcmejh42gg4afloo1k02ug6vtj4pi9f.apps.googleusercontent.com',
+  iosBundleId: 'com.inferloom.emiradriver',
+);
+
 
 }
